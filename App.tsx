1ca89cbd7c9b02a804f98dbe3a5afaf6b397fe8f
TangiManeux <tmaneux@ensc.fr>
2021-04-16 12:11:28 +0200

    app.tsx réparé

--- App.tsx
+++ App.tsx
@@ -1,21 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-<<<<<<< HEAD
-
-=======
 import { TabNavigator } from "./navigation/tab-navigation";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return <TabNavigator />;
->>>>>>> 6c04ec54ae7fd4c05f2f0d7389029f5912381262
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
