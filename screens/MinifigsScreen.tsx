40088e2b3bb82fc467bebb338f2dec31696865d2
Amandine Vallée <63154381+avallee002@users.noreply.github.com>
2021-04-16 11:24:02 +0200

    dependances

--- screens/MinifigsScreen.tsx
+++ screens/MinifigsScreen.tsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { Button, Text, View } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
-import { MinifigsScreenProps as MinifigsScreenProps } from "../navigation/app-stacks";
+import { MinifigsScreenProps} from "../navigation/app-stacks";
 import CocktailItem from "../components/SetsItem";
 
-export default class DetailsScreen extends Component<DetailsScreenProps, {}> {
+export default class MinifigsScreen extends Component<MinifigsScreenProps, {}> {
   render() {
     //const navigation: StackNavigationProp<RootStackParamList, "Cocktail">
     //const{navigation} = this.props;
